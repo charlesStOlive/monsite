4a2d837e3eb8a74b8562e3e6f24cdce94684232b
charles <charles.stolive@dayonmars.fr>
2019-03-19 17:23:09 +0100

    add expertise routes

--- plugins/charles/marketing/routes.php
+++ plugins/charles/marketing/routes.php
@@ -4,6 +4,7 @@ use Charles\Marketing\Models\Client;
 use Charles\Marketing\Models\Project;
 use Charles\Marketing\Models\Settings;
 use Charles\Marketing\Models\Competence;
+use Charles\Marketing\Models\Expertise;
 
 
 Route::options('api/clients', function() {
@@ -41,5 +42,14 @@ Route::options('api/project/{any}', function() {
 
 Route::get('api/project/{slug}', function($slug) {
 	$data = Project::where('slug', $slug)->with('main_picture', 'video_picture', 'pictures', 'client')->first();
+	return $data;
+});
+
+Route::options('api/expertises/{any}', function() {
+    return Response::make('You are connected to the API');
+});
+
+Route::get('api/expertises/{slug}', function($slug) {
+	$data = Expertise::where('slug', $slug)->with('competences', 'projects', 'projects.main_picture' )->first();
 	return $data;
 });
