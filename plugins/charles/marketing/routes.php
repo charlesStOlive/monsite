5f7df9cbbfaf95c091eb33612287c6ec9f757701
charles <charles.stolive@dayonmars.fr>
2019-03-22 17:02:15 +0100

    update plugins

--- plugins/charles/marketing/routes.php
+++ plugins/charles/marketing/routes.php
@@ -5,6 +5,7 @@ use Charles\Marketing\Models\Project;
 use Charles\Marketing\Models\Settings;
 use Charles\Marketing\Models\Competence;
 use Charles\Marketing\Models\Expertise;
+use Charles\Marketing\Models\Target;
 
 
 Route::options('api/clients', function() {
@@ -34,6 +35,7 @@ Route::get('api/settings', function() {
 	$data['projects'] = Project::with('main_picture', 'client')->get();
 	$data['clients'] = Client::get();
 	$data['competences'] = Competence::get();
+	$data['targets'] = Target::with('missions', 'missions.competences' )->get();
 	return $data;
 });
 Route::options('api/project/{any}', function() {
@@ -53,3 +55,22 @@ Route::get('api/expertises/{slug}', function($slug) {
 	$data = Expertise::where('slug', $slug)->with('competences', 'projects', 'projects.main_picture' )->first();
 	return $data;
 });
+
+Route::options('api/targets', function() {
+    return Response::make('You are connected to the API');
+});
+
+Route::get('api/targets', function($slug) {
+	$data = Target::with('competences', 'targets')->get();
+	return $data;
+});
+
+Route::options('api/targets/{any}', function() {
+    return Response::make('You are connected to the API');
+});
+
+Route::get('api/targets/{slug}', function($slug) {
+	$data = Target::where('slug', $slug)->with('missions', 'missions.competences')->first();
+	return $data;
+});
+
