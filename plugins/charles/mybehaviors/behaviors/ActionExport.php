c345cb55dab0ec19589183c1c4715bf0060b15e5
charles <charles.stolive@dayonmars.fr>
2019-11-14 11:44:45 +0100

    ajout clipboard manuel

--- plugins/charles/mybehaviors/behaviors/ActionExport.php
+++ plugins/charles/mybehaviors/behaviors/ActionExport.php
@@ -3,6 +3,7 @@
 use Backend\Classes\ControllerBehavior;
 use October\Rain\Exception\ApplicationException;
 use Charles\Mailgun\Models\Contact;
+use Charles\Mailgun\Models\Settings;
 
 class ActionExport extends ControllerBehavior
 {
@@ -16,6 +17,8 @@ class ActionExport extends ControllerBehavior
 
     public function onLoadActionExportForm()
     {
+        $this->vars['message_motivation'] = Settings::get('message_motivation');
+        
         $this->vars['keyClient'] = Contact::find(post('id'))->key;
         $this->vars['modelId'] = post('id');
         return $this->makePartial('$/charles/mybehaviors/behaviors/actionexport/_actionexport_form.htm');  
